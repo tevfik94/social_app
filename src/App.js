df37e32792acaa8923e2b434cff7751892d2ff98
Tevfik Istanbullu <t84177926@china.huawei.com>
2022-12-06 17:39:49 +0300

    3rd

--- src/App.js
+++ src/App.js
@@ -14,10 +14,10 @@ import Profile from "./pages/profile/Profile";
 import "./style.scss";
 import { DarkModeContext } from "./context/darkModeContext";
 import { useContext } from "react";
-// import { AuthContext } from "./context/authContext";
+import { AuthContext } from "./context/authContext";
 
 function App() {
-  const currentUser = true;
+  const { currentUser } = useContext(AuthContext);
   const { darkMode } = useContext(DarkModeContext);
 
   const Layout = () => {
